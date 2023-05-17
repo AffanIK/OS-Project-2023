73fa3c6aa6dcd8283db33e486704ebd4ab6b6ace
AyanKhan-3119 <127008532+AyanKhan-3119@users.noreply.github.com>
2023-05-17 14:31:35 +0500

    Update project.c

--- project.c
+++ project.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+#define NUM_THREADS 4
+#define MAX_NODES 100
+
+typedef struct Node {
+    int id;
+    int cost;
+    int heuristic;
+    struct Node* parent;
+} Node;
+
+typedef struct Edge {
+    int from;
+    int to;
+    int cost;
+} Edge;
+
+int num_nodes = 0;
+Node* nodes[MAX_NODES];
+int num_edges = 0;
+Edge edges[MAX_NODES * MAX_NODES];
+
+
+
+void addnode(int id, int heuristic) {
+    Node* node = malloc(sizeof(Node));
+    node->id = id;
+    node->heuristic = heuristic;
+    node->parent = NULL;
+    nodes[num_nodes++] = node;
+}
+
+void addedge(int from, int to, int cost) {
+    edges[num_edges++] = (Edge){ from, to, cost };
+}
